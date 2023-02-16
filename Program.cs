2348343a08dfdd4a33c37e1c1a473104c4ba979e
Zhenya <mrkv_ev@inbox.ru>
2023-02-16 11:56:07 +0300

    Начало)

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Friendship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello friend <3");
+        }
+    }
+}
