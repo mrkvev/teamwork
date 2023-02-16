d91c309c03e8e9e04251e324289d6e3b2f143af9
alice <alisa_barysheva03@mail.ru>
2023-02-16 12:10:39 +0300

    smth beautiful in this messeage

--- Program.cs
+++ Program.cs
@@ -8,7 +8,8 @@ namespace Friendship
         {
             Console.WriteLine("Hello friend <3");
 Console.WriteLine("Hello papapappapapa");
-Console.WriteLine("How are you?")
+Console.WriteLine("How are you?");
+Console.WriteLine("you are beautiful and cute. I love u");
         }
     }
 }
